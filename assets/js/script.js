8e6da0fd7bb6da991f3ad92acb5b08b8872079f5
Emilion <emiljadersten@gmail.com>
2023-12-08 13:55:54 +0100

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 console.log("Hello, World!")
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calcuLateCorrectAnswer() {
+    
+}
+
+function incrementScore () {
+
+}
+
+function incrementIncorrectAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
